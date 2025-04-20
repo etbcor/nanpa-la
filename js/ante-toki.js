781c3ba7ee7ea121272a259c3cb51ff6ed2e0c3d
etbcor <dev@etbcor.com>
2025-04-20 10:40:40 +0000

    open pi ante suli nasin

--- js/ante-toki.js
+++ js/ante-toki.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
+
   function oAnteEToki(toki) {
     const tenpo = new Date();
     tenpo.setDate(tenpo.getDate() + 30);
@@ -9,12 +10,35 @@ document.addEventListener('DOMContentLoaded', function() {
     return document.cookie.replace(/(?:(?:^|.*;\s*)toki\s*\=\s*([^;]*).*$)|^.*$/, "$1");
   }
 
+  function oPonaELipu(toki) {
+    if (toki === "en") {
+      document.documentElement.style.setProperty("--sp", "none");
+      document.documentElement.style.setProperty("--tp", "none");
+      document.documentElement.style.setProperty("--en", "block");
+    } else if (toki === "tp") {
+      document.documentElement.style.setProperty("--sp", "none");
+      document.documentElement.style.setProperty("--tp", "block");
+      document.documentElement.style.setProperty("--en", "none");
+    } else { // sp
+      document.documentElement.style.setProperty("--sp", "block");
+      document.documentElement.style.setProperty("--tp", "none");
+      document.documentElement.style.setProperty("--en", "none");
+    }
+  }
+  
+  let toki = oLukinEToki();
+  if (!toki) {
+    toki = "sp";
+    oAnteEToki(toki);
+  }
+  oPonaELipu(toki);
+
   const ijo = document.getElementById('ante-toki');
-  ijo.value = oLukinEToki();
+  ijo.value = toki;
 
   ijo.addEventListener('change', function() {
     const toki = this.value;
     oAnteEToki(toki);
-    location.reload();
+    oPonaELipu(toki);
   });
 });
