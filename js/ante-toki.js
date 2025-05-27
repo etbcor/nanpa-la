ba14219222f40801e84cb95fecf406319cb2e4c7
etbcor <dev@etbcor.com>
2025-05-27 13:46:33 +0000

    ante e sewi pi lipu ale. kin pana e lipu nasa

--- js/ante-toki.js
+++ js/ante-toki.js
@@ -1,36 +1,31 @@
-document.addEventListener('DOMContentLoaded', function() {
+function panaSona(nimi, sona) {
+  document.cookie = `${nimi}=${sona}; domain=.nanpa.la; path=/`;
+}
 
-  function panaSona(nimi, sona) {
-    document.cookie = `${nimi}=${sona}; domain=.nanpa.la; path=/`;
-  }
+function kamaSona(nimi) {
+  return document.cookie.split("; ").find((row) => row.startsWith(`${nimi}=`))?.split("=")[1];
+}
 
-  function kamaSona(nimi) {
-    return document.cookie.split("; ").find((row) => row.startsWith(`${nimi}=`))?.split("=")[1];
-  }
+function anteToki(toki) {
+  const s = document.documentElement.style;
+  function o(a, b) { return a.includes(b) ? "inline-block" : "none"; }
+  s.setProperty("--sp", o(toki, "sp"));
+  s.setProperty("--tp", o(toki, "tp"));
+  s.setProperty("--en", o(toki, "en"));
+}
 
-  function anteToki(toki) {
-    const s = document.documentElement.style;
-    function o(a, b) { return a.includes(b) ? "inline-block" : "none"; }
-    s.setProperty("--sp", o(toki, "sp"));
-    s.setProperty("--tp", o(toki, "tp"));
-    s.setProperty("--en", o(toki, "en"));
-  }
-  
-  let toki = kamaSona("toki");
-  if (!toki) {
-    toki = "sp";
-    panaSona("toki", toki);
-  }
+let toki = kamaSona("toki");
+if (!toki) {
+  toki = "sp";
+  panaSona("toki", toki);
+}
+anteToki(toki);
+
+const ijo = document.getElementById('ante-toki');
+ijo.addEventListener('change', function() {
+  const toki = this.value;
+  panaSona("toki", toki);
   anteToki(toki);
-
-  const ijo = document.getElementById('ante-toki');
-  ijo.value = toki;
-
-  ijo.addEventListener('change', function() {
-    const toki = this.value;
-    panaSona("toki", toki);
-    anteToki(toki);
-    this.style.setProperty("font-family", (toki === "sp" || toki === "sp,tp") ? "nasin-nanpa" : "Sans-serif");
-  });
-  ijo.style.setProperty("font-family", (toki === "sp" || toki === "sp,tp") ? "nasin-nanpa" : "Sans-serif");
+  this.style.setProperty("font-family", (toki === "sp" || toki === "sp,tp") ? "nasin-nanpa" : "Sans-serif");
 });
+ijo.value = toki;
