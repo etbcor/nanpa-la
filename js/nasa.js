e8abd69e9954688d27f3c3feeb96b329b3b25de7
etbcor <dev@etbcor.com>
2025-05-30 11:51:30 +0000

    ante e lipu 'alp' la ona li tawa lon leko (musi a)

--- js/nasa.js
+++ js/nasa.js
@@ -1,8 +1,10 @@
 function nasa() {
   const ken = [
+    "",
     "alp",
+    "nasitan",
     "ilo-pi-lawa-tawa-pi-suno-pi-kule-tu-wan",
   ];
   const kama = ken[Math.floor(Math.random() * ken.length)];
-  location.href = `https://nasa.nanpa.la/${kama}/`;
+  location.href = `https://nasa.nanpa.la/${kama}`;
 }
