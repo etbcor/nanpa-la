d4d65c6dbf449d24a14935325a653c9e28603909
etbcor <dev@etbcor.com>
2025-05-30 09:14:16 +0000

    ante e lipu pi ilo pi ken tawa pi ...

--- js/nasa.js
+++ js/nasa.js
@@ -1,10 +1,7 @@
 function nasa() {
   const ken = [
-    "0001",
-    "0002",
-    "0003",
     "alp",
-    "o-ken-ala-ken-tawa",
+    "ilo-pi-lawa-tawa-pi-suno-pi-kule-tu-wan",
   ];
   const kama = ken[Math.floor(Math.random() * ken.length)];
   location.href = `https://nasa.nanpa.la/${kama}/`;
