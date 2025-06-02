8baf5014d5cf02c921f0049a4d49c186ba3cff07
etbcor <dev@etbcor.com>
2025-06-02 21:49:25 +0000

    ijo open-en-pini li kama pali pona lon poki pi insa poki

--- js/open-en-pini.js
+++ js/open-en-pini.js
@@ -3,14 +3,28 @@ document.addEventListener('DOMContentLoaded', function() {
 
   for (const l of len) {
     l.addEventListener("toggle", function() {
-      const mama = this.parentElement.parentElement;
+      let mama = this.parentElement;
 
-      for (const c of mama.children) {
-        const cc = c.getElementsByTagName("details")[0];
-        if (cc !== this) {
-          cc.open = this.open;
+      if (mama.tagName === "TD") {
+        mama = mama.parentElement;
+        for (const c of mama.children) {
+          const cc = c.getElementsByTagName("details")[0];
+          if (cc !== this) {
+            cc.open = this.open;
+          }
+        }
+
+      } else if (mama.tagName === "DETAILS") {
+        const nanpa = Array.from(mama.children).indexOf(this);
+        mama = mama.parentElement.parentElement;
+        for (const c of mama.children) {
+          const cc = c.getElementsByTagName("details")[nanpa];
+          if (cc !== this) {
+            cc.open = this.open;
+          }
         }
       }
+
     });
   }
 });
