cbb4bede505ff234d7072539012d3c3c71e27a65
etbcor <dev@etbcor.com>
2025-07-06 10:02:29 +0000

    pana e lipu nasa pi toki Epanja

--- js/nasa.js
+++ js/nasa.js
@@ -4,6 +4,7 @@ function nasa() {
     "alp",
     "nasitan",
     "ilo-pi-lawa-tawa-pi-suno-pi-kule-tu-wan",
+    "tre",
   ];
   const kama = ken[Math.floor(Math.random() * ken.length)];
   location.href = `https://nasa.nanpa.la/${kama}`;
